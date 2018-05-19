a23b0e33fcab7f17aec9e60640f12699a812b2f2
Alan Golman <golman.alan@gmail.com>
2018-05-19 20:39:03 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,3 @@
 def prime?(n)
-  if n < 2
-    return false
-  else
-    array = (2..(n-1).to_a
-    array.each do |x|
-      if n % x == 0
-        return false
-      end
-    end
-  end
-  return true
-end 
+  
+end   
