6ed715bc045942844c878574a8b853068dd3d35b
Alan Golman <golman.alan@gmail.com>
2018-05-19 20:31:29 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,8 +3,8 @@ def prime?(n)
     return false
   else
     array = (2..(n-1).to_a
-    array.each do |x|
-      if n % x == 0
+    array.each do |d|
+      if n % d == 0
         return false
       end
     end
